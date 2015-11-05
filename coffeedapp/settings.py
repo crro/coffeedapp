5ef3c1a93eec53ad0da227ead90c6f67238a23af
David Correa Orozco <davidcroz1@gmail.com>
2015-11-04 16:54:19 -0800

    changed settings so that it works on heroku

--- coffeedapp/settings.py
+++ coffeedapp/settings.py
@@ -75,20 +75,19 @@ WSGI_APPLICATION = 'coffeedapp.wsgi.application'
 
 
 # Database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(MAIN_DIR, 'db.sqlite3'),
+    }
+}
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-if ON_HEROKU == True:
+if ON_HEROKU == '1':
     # Parse database configuration from $DATABASE_URL
     import dj_database_url
-
     DATABASES['default'] = dj_database_url.config()
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(MAIN_DIR, 'db.sqlite3'),
-        }
-}
+
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
